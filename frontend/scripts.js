54b062fea5a8b808959930eb32c94a83d19e3c8b
Sarah Badibanga <sarahbadibanga@Sarahs-MacBook-Pro.local>
2022-05-03 19:07:15 +0100

    working on responsive

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,23 +1,37 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-param-reassign */
 
 
 /*----------------------------------------
  nav js for animation starts here
  ----------------------------------------*/
-menuToggler.addEventListener('click', ev => {
-	menu.classList.toggle('open');
-	menuToggler.textContent = menuToggler.textContent === "×" ? "≡" : "×";
-});
+ const navSlide = () =>{
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+    // toggle Nav
+    burger.addEventListener('click', () =>{
+      nav.classList.toggle('nav-active');
+      // Animate links
+      navLinks.forEach((link, index) => {
+        if(link.style.animation ){
+          link.style.animation = ''
+        }else{
+          link.style.animation = `navLinkFade 0.5 ease forwards ${index/7 + 2.0}s`
+      }
+      });
+      // burger animation
+      burger.classList.toggle('toggle');
+    })
+ }
+ navSlide();
 /**
  * Enable hidden header
  */
  const nav = document.querySelector(".header");
  let lastScrollY = window.scrollY;
- const screensize = window.screen.width;
 
  window.addEventListener("scroll", () => {
-   if (lastScrollY< window.scrollY && screensize >= 768) {
+   if (lastScrollY< window.scrollY) {
      nav.classList.add("header--hidden");
    } else {
      nav.classList.remove("header--hidden");
